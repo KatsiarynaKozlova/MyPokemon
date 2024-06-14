61e67ff03d5fd9a720da193df9521d4b4dcff61c
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-06-14 14:14:18 +0300

    add binding, DetailsActivity

--- app/src/main/java/by/lab/mypokemon/DetailsActivity.kt
+++ app/src/main/java/by/lab/mypokemon/DetailsActivity.kt
@@ -2,14 +2,13 @@ package by.lab.mypokemon
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import by.lab.mypokemon.databinding.ActivityMainBinding
+import by.lab.mypokemon.databinding.ActivityDetailsBinding
 
-
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding:  ActivityMainBinding
+class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
