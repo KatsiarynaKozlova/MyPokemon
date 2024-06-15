a1637fbd301a3de49739504ea6fb917228fa0755
smthg wrong <94138707+KatsiarynaKozlova@users.noreply.github.com>
2024-06-16 00:26:44 +0300

    Update DetailsActivity.kt

--- app/src/main/java/by/lab/mypokemon/DetailsActivity.kt
+++ app/src/main/java/by/lab/mypokemon/DetailsActivity.kt
@@ -2,7 +2,9 @@ package by.lab.mypokemon
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import by.lab.mypokemon.databinding.ActivityDetailsBinding
 import by.lab.mypokemon.repository.PokemonRepository
@@ -15,7 +17,6 @@ class DetailsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val pokemonId = intent.extras?.getInt("pokemon_id", -1) ?: -1
-
         if (pokemonId != -1) {
             val pokemon = PokemonRepository.getPokemonById(pokemonId)
             if (pokemon != null) {
@@ -26,10 +27,14 @@ class DetailsActivity : AppCompatActivity() {
                 val adapter = ArrayAdapter(this@DetailsActivity, android.R.layout.simple_list_item_1, pokemon.elementalType )
                 binding.pokemonTypeListView.adapter = adapter
             } else {
+                Toast.makeText(this, "Unknown error. Try again later", Toast.LENGTH_SHORT).show()
                 Log.e("mainPage", "Pokemon with id not exist")
+                finish()
             }
         } else {
+            Toast.makeText(this, "Unknown error. Try again later", Toast.LENGTH_SHORT).show()
             Log.e("mainPage", "Invalid id")
+            finish()
         }
         binding.backBtn.setOnClickListener{
             finish()
